e609fa8964f54e59971fd84e152214cbdf5e3483
Colin Francis <francico@oregonstate.edu>
2021-07-02 14:49:15 -0400

    _in_row method added to search for a value in a specified row. _in_col method added to search for a value in a specified column. Docstrings added for both methods.

--- back_propagation_algorithm.py
+++ back_propagation_algorithm.py
@@ -13,14 +13,43 @@ class SolveSudoku(object):
         :return:
         """
         row_index, col_index = 0, 0
-        while True:
+        while '.' in self._puzzle.get_layout()[0]:
             if not self._puzzle.is_preset(row_index, col_index):
-                print(row_index, col_index)
-                input()
+                for number in range(1, 10):
+                    if not self._in_row(row_index, str(number)) and not self._in_col(col_index, str(number)):
+                        self._puzzle.set(row_index, col_index, str(number))
+                        self.print_board()
+                        break
             col_index += 1
 
     def _back_prop(self):
         pass
+
+    def _in_row(self, row_index: int, number: str) -> bool:
+        """
+        Searches the Sudoku puzzle to see if `number` exists in the specified row index.
+
+        :param row_index: The row index in the Sudoku puzzle to search in.
+        :param number: The number to search for.
+        :return: True if the number is found in the row. Otherwise, False.
+        """
+        if number in self._puzzle.get_row(row_index):
+            return True
+        return False
+
+    def _in_col(self, col_index: int, number: str) -> bool:
+        """
+        Searches the Sudoku puzzle to see if 'number' exists in the specified column
+        index.
+
+        :param col_index: The column index in the Sudoku puzzle to search in.
+        :param number: The number to search for.
+        :return: True if the number is found in the column. Otherwise, False.
+        """
+        for row in self._puzzle.get_layout():
+            if row[col_index] == number:
+                return True
+        return False
 
     def print_board(self):
         for row in self._puzzle.get_layout():
@@ -37,6 +66,12 @@ class Puzzle(object):
     def get_layout(self) -> list:
         """Returns the current layout of the Sudoku puzzle."""
         return self._layout
+
+    def get_row(self, row_index):
+        return self._layout[row_index]
+
+    def set(self, row_index, col_index, number):
+        self._layout[row_index][col_index] = number
 
     def is_preset(self, row_index: int, col_index: int) -> bool:
         """
