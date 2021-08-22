86e8eb559bfff96c5913047eedbb44de840f620a
Colin Francis <francico@oregonstate.edu>
2021-08-22 13:01:08 -0400

    Main.py added to handle launching the sudoku solver

---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Colin Francis
+# Description: Main file used to launch the sudoku solver
+
+from sudoku_solver import *
+
+if __name__ == "__main__":
+    solve = SudokuSolver()
+    solve.run()
